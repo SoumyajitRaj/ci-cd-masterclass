7f6aef70eeb266e02c0bd3d0e439aca6cc5746dc
Soumyajit Raj <soumyajitraj70@gmail.com>
2026-07-25 14:45:03 +0530

    /welcome/users endpoint added.

--- src/main/java/com/soumyait/com/controller/WelcomeController.java
+++ src/main/java/com/soumyait/com/controller/WelcomeController.java
@@ -1,5 +1,7 @@
 package com.soumyait.com.controller;
 
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/welcome")
 public class WelcomeController {
-	@GetMapping
-	public ResponseEntity<String> welcome(){
-		return new ResponseEntity<>("Welcome to Spring Boot App",HttpStatus.OK);
-	}
 
+    @GetMapping
+    public ResponseEntity<String> welcome() {
+        return new ResponseEntity<>("Welcome to Spring Boot App", HttpStatus.OK);
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<Map<String, Object>> getUser() {
+        return new ResponseEntity<>(
+                Map.of("name", "Substring technologies", "live", true),
+                HttpStatus.OK
+        );
+    }
 }
